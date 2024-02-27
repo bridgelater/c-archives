1860f7ac951b1f1d038f42bc204ac457d2520603
Bridget Lieder <161470168+bridgelater@users.noreply.github.com>
2024-02-27 15:01:39 +0200

    Create maincode.cpp

---
+++ maincode.cpp
@@ -0,0 +1,25 @@
+  if (!in.good()) {
+        std::cerr << "an error occured while reading the header\n";
+        return false;
+    }
+    w   = header.width;
+    h   = header.height;
+    bpp = header.bitsperpixel>>3;
+    if (w<=0 || h<=0 || (bpp!=GRAYSCALE && bpp!=RGB && bpp!=RGBA)) {
+        std::cerr << "bad bpp (or width/height) value\n";
+        return false;
+    }
+
+void Model::load_texture(std::string filename, const std::string suffix, TGAImage &img) {
+    size_t dot = filename.find_last_of(".");
+    if (dot==std::string::npos) return;
+    std::string texfile = filename.substr(0,dot) + suffix;
+    std::cerr << "texture file " << texfile << " loading " << (img.read_tga_file(texfile.c_str()) ? "ok" : "failed") << std::endl;
+}
+vec2 Model::uv(const int iface, const int nthvert) const {
+    return tex_coord[facet_tex[iface*3+nthvert]];
+}
+
+vec3 Model::normal(const int iface, const int nthvert) const {
+    return norms[facet_nrm[iface*3+nthvert]];
+}
